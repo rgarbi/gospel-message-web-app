e31b9e2772f16009d3221a2481db4db7895637bc
Richard Garbi <richard@mobilgov.com>
2022-05-03 06:27:50 -0500

    update staging location

--- src/util/serverLocation.js
+++ src/util/serverLocation.js
@@ -5,7 +5,7 @@ function getServerAddress() {
     let hostname = currentLocation.hostname;
 
     if(hostname.includes('newsletter-web-app-cxqn.onrender.com')) {
-        return 'https://newsletter-signup-service-r8s5.onrender.com';
+        return 'https://newsletter-signup-service-r8s5-cvp4.onrender.com';
     }
 
     if(hostname.includes('newsletter-web-app.onrender.com')) {
