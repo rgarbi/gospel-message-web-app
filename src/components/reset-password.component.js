b726e4c3cde90bcd96b3efa61be13996cd0aa691
Richard Garbi <richard@mobilgov.com>
2022-05-11 16:00:48 -0500

    update styling and fix another wanring

--- src/components/reset-password.component.js
+++ src/components/reset-password.component.js
@@ -6,13 +6,18 @@ import getServerAddress from '../util/serverLocation';
 import { exchangeOtpForToken, forgotPasswordResetPassword } from '../api/client';
 import { clearToken } from '../store/auth/token';
 
+import Container  from 'react-bootstrap/Container';
+import Card  from 'react-bootstrap/Card';
+import Col  from 'react-bootstrap/Col';
+import Row  from 'react-bootstrap/Row';
+
 
 export default function ResetPassword() {
   const [password, setPassword] = useState('');
   const [userId, setUserId] = useState('');
   const [tempToken, setTempToken] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
 
   const dispatch = useDispatch();
   let navigate = useNavigate();
@@ -31,7 +36,6 @@ export default function ResetPassword() {
         const response = await exchangeOtpForToken(address, otp);
 
         if(response.statusCode > 300) {
-          console.log('yup, bad code');
           setErrorMessage('Something something try again.')
         }
 
@@ -40,12 +44,9 @@ export default function ResetPassword() {
           setTempToken(token.token);
           setUserId(token.user_id);
         }
-
       }
-      
     };
 
-    
     validateOTP();
     
   }, [navigate, searchParams]);
@@ -72,25 +73,33 @@ export default function ResetPassword() {
 
 
   return (
-      <form  onSubmit={handleSubmit}>
-        <div className='form-group'>
-          <label>Password</label>
-          <input
-            type='password'
-            className='form-control'
-            placeholder='Enter password'
-            onChange={evt => setPassword(evt.target.value)}
-            value={password}
-            id='password'
-          />
-        </div>
-        <p></p>
-        <div>{errorMessage}</div>
-        <p></p>
-        <button type='submit' className='btn btn-primary btn-block'>
-          Submit
-        </button>
-      </form>
+          <Container className="p-5 ">
+          <Row>
+              <Col >
+                  <Card className="p-5 border border-light rounded-3 text-start">
+                    <form  onSubmit={handleSubmit}>
+                      <div className='form-group'>
+                        <label>Password</label>
+                        <input
+                          type='password'
+                          className='form-control'
+                          placeholder='Enter password'
+                          onChange={evt => setPassword(evt.target.value)}
+                          value={password}
+                          id='password'
+                        />
+                      </div>
+                      <p></p>
+                      <div>{errorMessage}</div>
+                      <p></p>
+                      <button type='submit' className='btn btn-primary btn-block'>
+                        Submit
+                      </button>
+                    </form>
+                  </Card>
+              </Col>
+          </Row>
+      </Container>
     );
   
 }
